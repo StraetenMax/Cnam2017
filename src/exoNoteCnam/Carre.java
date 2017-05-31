fef23d4f70bd7d77febb08cd6d9fe09242fa2157
StraetenMax <maxstraeten@yahoo.com>
2017-05-31 14:26:58 +0200

    classe Carre méthodes

--- src/exoNoteCnam/Carre.java
+++ src/exoNoteCnam/Carre.java
@@ -3,20 +3,53 @@ package exoNoteCnam;
 public class Carre extends Quadrilatere{
 	//propriétés
 	private int tailleCote;
-	private Boolean grand;
+	private boolean grand;
 	
 	//constructeurs vide et initialisé
 	public Carre(){
 		super();
 		tailleCote = 0;
-		grand = true;
+		this.setGrand();
 	}
 	public Carre(int surface, String couleur, String type, int tailleCote){
 		super(surface, couleur,type);
 		this.tailleCote = tailleCote;
-		grand = true;
+		this.setGrand();
 	}
 	
 	//accesseurs et mutateurs
+	public int getTailleCote(){
+		return tailleCote;
+	}
+	public void setTailleCote(int tailleCote){
+		this.tailleCote = tailleCote;
+		this.setGrand();
+	}
+	public boolean getGrand(){
+		return grand;
+	}
+	public void setGrand(){
+		if(tailleCote > 10){
+			grand = true;
+		}else{
+			grand = false;
+		}
+	}
+	//méthodes
+	public boolean estPlusGrandQue(Carre str){
+		boolean str1;
+		if(this.tailleCote >str.tailleCote){
+			str1 = true;
+		}else{
+			str1 = false;
+		}
+		return str1;
+	}
+	
+	public String description(){
+		String str2 = super.description()+" dont le coté mesure "+this.getTailleCote()+" cm";
+		return str2;
+	}
+
 
 }
