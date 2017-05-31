4c19c1f9ce09f4e8e013b83318fc6bd290708fb9
StraetenMax <maxstraeten@yahoo.com>
2017-05-31 13:32:11 +0200

    class Forme méthodes

--- src/exoNoteCnam/Forme.java
+++ src/exoNoteCnam/Forme.java
@@ -15,5 +15,35 @@ public class Forme {
 		this.surface = surface;
 		this.couleur = couleur;
 	}
-
+	
+	//accesseurs et mutateurs
+	public int getSurface(){
+		return surface;
+	}
+	
+	public void setSurface(int surface){
+		this.surface = surface;
+	}
+	
+	public String getCouleur(){
+		return couleur;
+	}
+	
+	public void setCouleur(String couleur){
+		this.couleur = couleur;
+	}
+	
+	//méthodes
+	public String afficher(){
+		String str = "Forme affichée";
+		return str;	
+	}
+	public String masquer(){
+		String str1 = "Forme masquée";
+		return str1;
+	}
+	public String description(){
+		String str2 = "La forme de surface "+this.getSurface()+" est de couleur "+this.getCouleur();
+		return str2;
+	}
 }
