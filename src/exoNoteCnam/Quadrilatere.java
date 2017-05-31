6492f46663b47cacf3ee8203d9b813a2193c2c3e
StraetenMax <maxstraeten@yahoo.com>
2017-05-31 13:42:26 +0200

    classe Quadrilatere

---
+++ src/exoNoteCnam/Quadrilatere.java
@@ -0,0 +1,16 @@
+package exoNoteCnam;
+
+public class Quadrilatere extends Forme{
+	//propriétés
+	private String type;
+	
+	//constructeurs vide et initialisé
+	public Quadrilatere(){
+		super();
+		type = "";
+	}
+	public Quadrilatere(int surface, String couleur, String type){
+		super(surface, couleur);
+		this.type = type;
+	}
+}
